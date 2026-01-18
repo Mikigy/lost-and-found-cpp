b88b6b46ade74a4ceabe2ca1720fc106487ccb5f
mekdes842 <mekdestesfayey@gmail.com>
2026-01-18 16:34:07 +0300

    Implement search functionality

---
+++ include/search.h
@@ -0,0 +1,6 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+void searchItem(char[][20], char[][20], char[][20], char[][15], int*);
+
+#endif
---
+++ src/search.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include "search.h"
+#include "utils.h"
+
+using namespace std;
+
+extern int countItems;
+
+void searchItem(char name[][20], char color[][20],
+                char location[][20], char date[][15], int* claimed) {
+
+    char search[20], temp[20];
+    bool found = false;
+
+    cout << "Enter item name: ";
+    cin >> search;
+    toLower(search);
+
+    for (int i = 0; i < countItems; i++) {
+        if (!claimed[i]) {
+            strcpy(temp, name[i]);
+            toLower(temp);
+            if (strstr(temp, search)) {
+                cout << "Item: " << name[i] << "\nColor: " << color[i]
+                     << "\nLocation: " << location[i]
+                     << "\nDate: " << date[i] << "\n\n";
+                found = true;
+            }
+        }
+    }
+
+    if (!found) cout << "No item found.\n";
+}
