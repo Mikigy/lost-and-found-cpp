dcda793443be91e2b60f8948123fd8f904ea806b
manna263 <menichdav@gmail.com>
2026-01-19 11:03:13 -0800

    Add item management module

---
+++ include/item management.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+#include "claim.h"
+
+using namespace std;
+
+extern int countItems;
+
+void claimItem(int* id, char secret[][20], char claimer[][30],
+               char dateClaimed[][15], int* claimed) {
+
+    int searchId;
+    char userSecret[20];
+
+    cout << "Enter Item ID: ";
+    cin >> searchId;
+
+    for (int i = 0; i < countItems; i++) {
+        if (id[i] == searchId && !claimed[i]) {
+            cout << "Enter secret mark: ";
+            cin >> userSecret;
+
+            if (strcmp(secret[i], userSecret) == 0) {
+                cin.ignore();
+                cout << "Claimer Name: ";
+                cin.getline(claimer[i], 30);
+                cout << "Date Claimed: ";
+                cin >> dateClaimed[i];
+                claimed[i] = 1;
+                cout << "Item claimed!\n";
+            } else {
+                cout << "Wrong secret.\n";
+            }
+            return;
+        }
+    }
+    cout << "Invalid ID.\n";
+}
---
+++ src/menu.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "menu .h"
+ void showMenu()
+     std::cout <<"1 . ADD item\n";
+     std::cout <<"2 . view items\n";
